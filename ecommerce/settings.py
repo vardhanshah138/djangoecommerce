fd0a40ef7f189dbbc66ab6a127f85c0e7804da06
vardhanshah138 <vardhanshah07@gmail.com>
2020-06-25 17:34:28 +0530

    database changed to postgresql

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',  # we have to change engine name
+        'NAME': 'TodoDB',  # and provide name of database we are using
+        'USER': 'postgres',
+        'PASSWORD': 'postgresql',
+        'HOST': 'localhost',
     }
+
 }
 
 
